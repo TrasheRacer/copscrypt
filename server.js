bbeb3a85f0761f5c10eb864e26fadd5fe175dae9
Jake Ashdown <jake.ashdown@pm.me>
2024-08-06 20:34:12 +0200

    establish socket.io connection

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
+// public
+
 import express from 'express';
 import { createServer } from 'node:http';
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
+import { Server } from 'socket.io';
+
+// private
 
 import { log } from './server-log.mjs'
 
 const app = express();
 const server = createServer(app);
+
+// initialise socket.io
+const io = new Server(server);
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -21,6 +29,11 @@ app.get('/stream', (req, res) => {
     log('requested stream')
     res.sendFile(join(__dirname, 'index-stream.html'));
 });
+
+// event predefined by library
+io.on('connection', (socket) => {
+    log('a user connected');
+  });
 
 server.listen(3000, () => {
     log('server running at http://localhost:3000');
