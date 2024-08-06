daeef5051cee239d26f49aaf0b83d7a1d24da355
Jake Ashdown <jake.ashdown@pm.me>
2024-08-06 21:02:24 +0200

    setup HTTP connection

--- README.md
+++ README.md
@@ -4,8 +4,13 @@
 
 ### Quickstart
 
+Find and replace example server IP address: `192.168.178.103`
+
+0. Generate a self-signed certificate for the correct host with `openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 3650 -nodes -subj "/C=XX/ST=StateName/L=CityName/O=CompanyName/OU=CompanySectionName/CN=192.168.178.103"` in `ssl/`
 1. Install with `pnpm`
 2. Run with `node server.js`
+3. Import certificate into browser for all peers
+4. Go to `https://192.168.178.103:3000/`
 
 ### Styleguide
 
--- server.js
+++ server.js
@@ -1,30 +1,30 @@
 // public
-
 import express from 'express';
-import { createServer } from 'node:http';
+import { readFileSync } from 'fs'
+import { createServer } from 'node:https';
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
 import { Server } from 'socket.io';
 
 // private
-
 import { log } from './server-log.mjs'
 
+// initialise
 const app = express();
-const server = createServer(app);
-
-// initialise socket.io
+const credentials = {
+    key: readFileSync("ssl/key.pem"),
+    cert: readFileSync("ssl/cert.pem")
+}
+const server = createServer(credentials, app);
 const io = new Server(server);
 
+// routes
 const __dirname = dirname(fileURLToPath(import.meta.url));
-
 app.use(express.static('public'))
-
 app.get('/', (req, res) => {
     log('requested index')
     res.sendFile(join(__dirname, 'index.html'));
 });
-
 app.get('/stream', (req, res) => {
     log('requested stream')
     res.sendFile(join(__dirname, 'index-stream.html'));
@@ -35,6 +35,7 @@ io.on('connection', (socket) => {
     log('a user connected');
   });
 
+// ready
 server.listen(3000, () => {
-    log('server running at http://localhost:3000');
+    log('listening');
 });
